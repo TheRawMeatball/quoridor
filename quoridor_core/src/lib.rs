c04e18d33b58c2f951c88a42e60bf18c2e06376c
TheRawMeatball <therawmeatball@gmail.com>
2020-08-31 20:50:19 +0300

    Switched to arrays and hashmaps

--- quoridor_core/src/lib.rs
+++ quoridor_core/src/lib.rs
@@ -1,7 +1,8 @@
 use serde::{Deserialize, Serialize};
 use tbmp::*;
+use std::collections::hash_set::HashSet;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Copy, Clone, Hash, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Position {
     pub x: u8,
     pub y: u8,
@@ -13,7 +14,7 @@ impl From<(u8, u8)> for Position {
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[derive(Copy, Clone, Hash, Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum Wall {
     Horizontal(Position),
     Vertical(Position),
@@ -27,90 +28,118 @@ pub enum Move {
 
 #[derive(Clone, Serialize, Deserialize)]
 pub struct Quoridor {
-    pub wall_counts: Vec<u8>,
-    pub pawn_positions: Vec<Position>,
-    pub walls: Vec<Wall>,
+    pub wall_counts: [u8; 2],
+    pub pawn_positions: [Position; 2],
+    pub walls: HashSet<Wall>,
     pub turn_of: PlayerID,
     pub player_count: u8,
 }
 
+impl Quoridor {
+    fn check_movable(
+        &self,
+        pawn_pos: Position,
+        pos: Position,
+    ) -> Result<(), ()> {
+        match (
+            pawn_pos.x as i8 - pos.x as i8,
+            pawn_pos.y as i8 - pos.y as i8,
+        ) {
+            (x, 0) => {
+                if x.abs() > 1 || x == 0 {
+                    Err(())
+                } else {
+                    if self
+                        .walls
+                        .contains(&Wall::Vertical((pos.x + (x > 0) as u8, pos.y).into()))
+                        || self
+                            .walls
+                            .contains(&Wall::Vertical((pos.x + (x > 0) as u8, pos.y + 1).into()))
+                    {
+                        Err(())
+                    } else {
+                        Ok(())
+                    }
+                }
+            }
+            (0, y) => {
+                if y.abs() > 1 || y == 0 {
+                    Err(())
+                } else {
+                    if self
+                        .walls
+                        .contains(&Wall::Horizontal((pos.x, pos.y + (y > 0) as u8).into()))
+                        || self
+                            .walls
+                            .contains(&Wall::Horizontal((pos.x + 1, pos.y + (y > 0) as u8).into()))
+                    {
+                        Err(())
+                    } else {
+                        Ok(())
+                    }
+                }
+            }
+            _ => Err(()),
+        }
+    }
+}
+
 impl Game for Quoridor {
+    const PLAYER_COUNT: u8 = 2;
     type Move = Move;
 
     fn validate_move(&self, qmove: Move) -> Result<(), ()> {
         match qmove {
             Move::PlaceWall(wall) => match wall {
                 Wall::Horizontal(pos) => {
-                    if !self.walls.contains(&Wall::Vertical(pos))
-                        && !self
-                            .walls
-                            .contains(&Wall::Horizontal((pos.x.wrapping_sub(1), pos.y).into()))
-                        && !self
-                            .walls
-                            .contains(&Wall::Horizontal((pos.x + 1, pos.y).into()))
-                    {
-                        Ok(())
-                    } else {
+                    if pos.x == 0 || pos.x == 8 || pos.y == 0 || pos.y == 8 {
                         Err(())
+                    } else {
+                        if !self.walls.contains(&Wall::Vertical(pos))
+                            && !self
+                                .walls
+                                .contains(&Wall::Horizontal((pos.x.wrapping_sub(1), pos.y).into()))
+                            && !self
+                                .walls
+                                .contains(&Wall::Horizontal((pos.x + 1, pos.y).into()))
+                        {
+                            Ok(())
+                        } else {
+                            Err(())
+                        }
                     }
                 }
                 Wall::Vertical(pos) => {
-                    if !self.walls.contains(&Wall::Horizontal(pos))
-                        && !self
-                            .walls
-                            .contains(&Wall::Vertical((pos.x, pos.y.wrapping_sub(1)).into()))
-                        && !self
-                            .walls
-                            .contains(&Wall::Vertical((pos.x, pos.y + 1).into()))
-                    {
-                        Ok(())
-                    } else {
+                    if pos.x == 0 || pos.x == 8 || pos.y == 0 || pos.y == 8 {
                         Err(())
+                    } else {
+                        if !self.walls.contains(&Wall::Horizontal(pos))
+                            && !self
+                                .walls
+                                .contains(&Wall::Vertical((pos.x, pos.y.wrapping_sub(1)).into()))
+                            && !self
+                                .walls
+                                .contains(&Wall::Vertical((pos.x, pos.y + 1).into()))
+                        {
+                            Ok(())
+                        } else {
+                            Err(())
+                        }
                     }
                 }
             },
             Move::MovePawn(pawn_id, pos) => {
                 let pawn_pos = self.pawn_positions[pawn_id as usize];
-                if let Ok(_) =
-                    match (
-                        pawn_pos.x as i8 - pos.x as i8,
-                        pawn_pos.y as i8 - pos.y as i8,
-                    ) {
-                        (x, 0) => {
-                            if x.abs() > 1 || x == 0 {
-                                Err(())
-                            } else {
-                                if self.walls.contains(&Wall::Vertical(
-                                    (pos.x + (x > 0) as u8, pos.y).into(),
-                                )) || self.walls.contains(&Wall::Vertical(
-                                    (pos.x + (x > 0) as u8, pos.y + 1).into(),
-                                )) {
-                                    Err(())
-                                } else {
-                                    Ok(())
-                                }
-                            }
-                        }
-                        (0, y) => {
-                            if y.abs() > 1 || y == 0 {
-                                Err(())
-                            } else {
-                                if self.walls.contains(&Wall::Horizontal(
-                                    (pos.x, pos.y + (y > 0) as u8).into(),
-                                )) || self.walls.contains(&Wall::Horizontal(
-                                    (pos.x + 1, pos.y + (y > 0) as u8).into(),
-                                )) {
-                                    Err(())
-                                } else {
-                                    Ok(())
-                                }
-                            }
-                        }
-                        _ => Err(()),
-                    }
+                if let Ok(_) = self.check_movable(pawn_pos, pos)
                 {
-                    //TODO: PATHFINDING
-                    Ok(())
+                    if self.pawn_positions.contains(&pawn_pos) {
+                        Err(())
+                    }
+                    else {
+                        //TODO: PATHFINDING
+
+                        Ok(())
+                    }
                 } else {
                     Err(())
                 }
@@ -121,7 +150,7 @@ impl Game for Quoridor {
     fn apply_move(&mut self, qmove: Move) -> MoveResult {
         match qmove {
             Move::PlaceWall(wall) => {
-                self.walls.push(wall);
+                self.walls.insert(wall);
             }
             Move::MovePawn(pawn_id, movement) => {
                 self.pawn_positions[pawn_id as usize] = movement;
@@ -136,16 +165,12 @@ impl Game for Quoridor {
 
     fn default_board() -> Self {
         Quoridor {
-            wall_counts: vec![10, 10],
-            pawn_positions: vec![(4, 0).into(), (4, 8).into()],
-            walls: vec![Wall::Horizontal((2, 2).into())],
+            wall_counts: [10, 10],
+            pawn_positions: [(4, 0).into(), (4, 8).into()],
+            walls: HashSet::new(),
             turn_of: 0,
             player_count: 2,
         }
-    }
-
-    fn player_count(&self) -> u8 {
-        self.player_count
     }
 
     fn turn_of(&self) -> u8 {
