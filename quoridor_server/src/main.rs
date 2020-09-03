b17de1ca1eef05ed018fee36f81aab50658a2c87
TheRawMeatball <therawmeatball@gmail.com>
2020-09-03 16:53:32 +0300

    Minor change

--- quoridor_server/src/main.rs
+++ quoridor_server/src/main.rs
@@ -7,8 +7,9 @@ type Quoridor = StandardQuoridor;
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
-    
+
     loop {
+        println!("NEW GAME");
         let (cores, mut game_thread) = tbmp::new_game::<Quoridor>();
         let mut player_threads = tbmp::remote_agent::host(cores, args[2].parse().unwrap());
         loop {
