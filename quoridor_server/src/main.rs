ad9f761a0edcf3db528d1252ef19ed8d53e13078
TheRawMeatball <therawmeatball@gmail.com>
2020-09-03 16:49:24 +0300

    added continuous matches to server

--- quoridor_server/src/main.rs
+++ quoridor_server/src/main.rs
@@ -6,17 +6,20 @@ use std::env;
 type Quoridor = StandardQuoridor;
 
 fn main() {
-    let (cores, mut game_thread) = tbmp::new_game::<Quoridor>();
     let args = env::args().collect::<Vec<_>>();
-    let mut player_threads = tbmp::remote_agent::host(cores, args[2].parse().unwrap());
+    
     loop {
-        let x = game_thread();
-        for t in player_threads.iter_mut() {
-            t().ok();
-        }
-        match x {
-            Ok(MoveResult::Continue) => {}
-            _ => return,
+        let (cores, mut game_thread) = tbmp::new_game::<Quoridor>();
+        let mut player_threads = tbmp::remote_agent::host(cores, args[2].parse().unwrap());
+        loop {
+            let x = game_thread();
+            for t in player_threads.iter_mut() {
+                t().ok();
+            }
+            match x {
+                Ok(MoveResult::Continue) => {}
+                _ => break,
+            }
         }
     }
 }
